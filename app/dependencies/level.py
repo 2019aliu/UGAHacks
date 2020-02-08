4e9033c30521f08d08aa0cbb90cf48fe2bce980c
2019aliu <aliu338@gatech.edu>
2020-02-08 16:33:59 -0500

    hungggggggg

---
+++ app/dependencies/level.py
@@ -0,0 +1,10 @@
+class Level:
+    def __init__(self, numDays, startDate, threshold):
+        self.numDays = numDays
+        self.startDate = startDate
+        self.threshold = threshold
+
+    def getNumDays(self):
+        return self.numDays
+    
+    def setNumDays(self, newNaps)
---
+++ app/test.py
@@ -0,0 +1,4 @@
+from dependencies.level import *
+
+testL = Level(365, "8/6/09", 15000)
+print(testL.getNumDays())
