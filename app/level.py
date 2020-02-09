cee5e3a15765d61bfc9597038e525e55165c1922
akim397 <andrewk102220@gmail.com>
2020-02-08 23:54:05 -0500

    hopefully

--- app/level.py
+++ app/level.py
@@ -11,4 +11,4 @@ class Level:
     	self.numDays = newNaps
 
     def getThreshold():
-    	return threshold
+    	return self.threshold
--- app/user.py
+++ app/user.py
@@ -1,52 +1,56 @@
 import stock
+import level
 
 class User:
-	def __init__(self, stockList)
+	def __init__(self, getStockList)
 		self.liquidAssets = 10000
 		self.stocksBought = {stock: 0 for stock in stockList}
 		self.stockShorted = {stock: 0 for stock in stockList}
+		self.updatedStockValues = {(stock): Stock(updatedStockValues[(stock)]).getPrice() for stock in stockList}
 		self.totalAssets = 0
 		self.done = false
 		self.success = false
+		self.short = 0;
 
-	def getStockList():
+	def getStockList(self):
 		return stocksBought
 
-	def buyStock(stock):
-		if (liquidAssets >= stock.getPrice()):
-			stocksBought[stock.ticker] = stocksBought[stock.ticker] + 1
-			liquidAssets = liquidAssets - stock.getPrice()
+	def buyStock(self, stock):
+		if (self.liquidAssets >= stock.getPrice()):
+			self.stocksBought[stock.ticker] += 1
+			self.liquidAssets = self.liquidAssets - stock.getPrice()
+			self.short++
 
-	def buyShort(stock):
-		if (liquidAssets >= stock.getPrice()):
-			stocksShorted[stock.ticker] = stocksShorted[stock.ticker] + 1
+	def buyShort(self, stock):
+		self.stocksShorted[stock.ticker] += 1
+		self.liquidAssets = self.liquidAssets + self.stockShorted[stock.ticker].getPrice()
 
-	def sellStock(stock):
-		if (stocksBought[stock.ticker] >= 1):
-			stocksBought[stock.ticker] = stocksBought[stock.ticker] - 1
-			liquidAssets = liquidAssets + stock.getPrice()
+	def sellStock(self, stock):
+		if (self.stocksBought[stock.ticker] >= 1):
+			self.stocksBought[stock.ticker] -= 1
+			self.liquidAssets = self.liquidAssets + stock.getPrice()
 
-	def sellShort(stock):
-		if (stockShorted[stock.ticker] >= 1):
-			stocksShorted[stock.ticker] = stocksShorted[stock.ticker] - 1
+	def sellShort(self, stock):
+		if (self.stockShorted[stock.ticker] >= 1 and self.liquidAssets >= stock.getPrice()):
+			self.stocksShorted[stock.ticker] -= 1
+			self.liquidAssets = self.liquidAssets - self.stockShorted[stock.ticker].getPrice()
+			self.short--
 
-	def getTotalAssets():
+	def getTotalAssets(self):
 		listTick = ['BLK', 'AAPL', 'NYT', 'DIS', 'GE', 'JPM', 'MSFT']
     	count = 0
-    	totalAssets = liquidAssets
+    	self.totalAssets = self.liquidAssets
     	while (count < len(listTick)):
-			totalAssets = totalAssets + stocksBought[listTick[count]]
+			self.totalAssets = self.totalAssets + self.stocksBought[listTick[count]]
+			if self.short > 0:
+				self.totalAssets -= self.stocksShorted[listTick[count]] * 0.05 * self.updatedStockValues[listTick[count]]
 			count++
-		return totalAssets
+		return self.totalAssets
 
-	def checkSuccessAndTime(stock1):
+	def checkSuccessAndTime(self, level):
 		count = 0
 		getTotalAssets()
-		if (totalAssets > stock1.getThreshold()):
-			success = true
-			done = true
-		totalAssets = 0
-
-
-user = new User()
-print(user.liquidAssets)
+		if (self.totalAssets > level.getThreshold()):
+			self.success = true
+			self.done = true
+		self.totalAssets = 0
